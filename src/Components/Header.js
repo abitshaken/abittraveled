67f54af007e3ec2b7509b031541293522904c9e5
Abhi K <projectleantea@gmail.com>
2020-08-05 00:37:36 -0400

    updating website

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Header = ({mainData}) => {
     if(mainData){
         var name = mainData.name;
+        var email = mainData.email;
         var occupation= mainData.occupation;
-        var description= mainData.description;
         var city= mainData.city;
         var networks= mainData.social.map(function(network){
             return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>;
@@ -17,7 +17,7 @@ const Header = ({mainData}) => {
         <div className="row banner">
            <div className="banner-text">
               <h1 className="responsive-headline">My name is {name}.</h1>
-              <h3>I'm a <span>{city}</span> based <span>{occupation}</span> {description}.</h3>
+              <h3>I'm a <span>{city}</span> based <span>{occupation}</span>. Shoot me an email at <span>{email}</span> or let's connect on social media.</h3>
               <hr />
               <ul className="social">{networks}</ul>
            </div>
