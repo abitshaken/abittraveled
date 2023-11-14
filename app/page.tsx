8f9b98f06f798a4ab0ffecf86b13edd82b496f10
Abhi <projectleantea@gmail.com>
2023-11-14 13:35:30 -0500

    Add email to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-flex-start p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <header className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex selection:text-white">
         Abhi Krishnan
-      </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-white/50">
+      </header>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-white/50 selection:text-white/50">
         Fullstack Engineer
+      </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-[purple]/60 selection:text-[purple]/60">
+        krabhishaken AT gmail DOT com
       </div>
     </main>
   )
